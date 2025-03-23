e31b8c6e0d33fcb5c798265046b3fc17ac5ef5c2
Laura-lorrayne <lauralorrayne24@gmail.com>
2025-03-23 17:24:58 -0300

    Feat: criacao menu de navegação

---
+++ src/components/Sidebar/MainNavigation/index.tsx
@@ -0,0 +1,21 @@
+import {
+    Home,
+    BarChart,
+    SquareStack,
+    CheckSquare,
+    Users,
+    Flag,
+} from "lucide-react";
+import { NavItem } from "./Navitem";
+export function MainNavigation() {
+    return (
+        <nav className="space-y-0.5">
+            <NavItem title="Home" icon={Home} />
+            <NavItem title="Dashboard" icon={BarChart} />
+            <NavItem title="Projects" icon={SquareStack} />
+            <NavItem title="Tasks" icon={CheckSquare} />
+            <NavItem title="Reporting" icon={Flag} />
+            <NavItem title="Users" icon={Users} />
+        </nav>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        laura:"#8257e6",
-        primary: "#3490dc",
-        secondary: "#f1c40f",
-        tertiary: "#e74c3c",
-        quaternary: "#2ecc71",
-      },
-      fontFamily: {
-        body: ["Inter", "system-ui", "sans-serif"],
-      },
-      maxWidth: {
-        md: "720px",
-        xl: "1280px",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                app: "minmax(18rem, 20rem) 1fr",
+                form: "minmax(7.5rem, 17.5rem), minmax(25rem, 1fr), minmax(0, 15rem)",
+            },
+
+            borderWidth: {
+                6: "6px",
+            },
+
+            colors: {
+                violet: {
+                    25: "#FCFAFF",
+                },
+
+                error: {
+                    25: "#FFFBFA",
+                    50: "#FEF3F2",
+                    100: "#FEE4E2",
+                    200: "#FECDCA",
+                    300: "#FDA29B",
+                    400: "#F97066",
+                    500: "#F04438",
+                    600: "#D92D20",
+                    700: "#B42318",
+                    800: "#912018",
+                    900: "#7A271A",
+                },
+            },
+
+            keyframes: {
+                slideDownAndFade: {
+                    from: { opacity: 0, transform: "translateY(-2px)" },
+                    to: { opacity: 1, transform: "translateY(0)" },
+                },
+                slideUpAndFade: {
+                    from: { opacity: 0, transform: "translateY(2px)" },
+                    to: { opacity: 1, transform: "translateY(0)" },
+                },
+            },
+
+            animation: {
+                slideDownAndFade:
+                    "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+                slideUpAndFade:
+                    "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+};
